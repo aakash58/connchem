cda39cd37a626359a7cf8c26bc43e60918b6447a
Tuan Dang <nhontuan@gmail.com>
2011-05-18 12:18:14 -0500

    Change interface in Main.java

---
+++ src/view/Boundary.java
@@ -0,0 +1,42 @@
+package view;
+
+import processing.core.*;
+import p5.Area;
+import pbox2d.*;
+import p5.Region;
+
+import org.jbox2d.common.*;
+import org.jbox2d.collision.shapes.*;
+import org.jbox2d.dynamics.*;
+
+public class Boundary extends Area{
+
+	private Area parent;
+	// But we also have to make a body for box2d to know about it
+	Body b;
+	PBox2D box2d;
+
+	Boundary(float x_,float y_, float w_, float h_, PBox2D box2d_, Area parent_) {
+		this.parent = parent_;
+		this.box2d = box2d_;
+		setDimensions(x_, y_, w_, h_);
+
+		// Figure out the box2d coordinates
+		float box2dW = box2d.scalarPixelsToWorld(w()/2);
+		float box2dH = box2d.scalarPixelsToWorld(h()/2);
+		Vec2 center = new Vec2(x(),y());
+
+		// Define the polygon
+		PolygonDef sd = new PolygonDef();
+		sd.setAsBox(box2dW, box2dH);
+		sd.density = 0;    // No density means it won't move!
+		sd.friction = 0.3f;
+
+		// Create the body
+		BodyDef bd = new BodyDef();
+		bd.position.set(box2d.coordPixelsToWorld(center));
+		b = box2d.createBody(bd);
+		b.createShape(sd);
+		b.setUserData(this);
+	}
+}
---
+++ src/view/P5Canvas.java
@@ -0,0 +1,131 @@
+package view;
+
+import java.util.ArrayList;
+
+import pbox2d.*;
+import org.jbox2d.collision.shapes.Shape;
+import org.jbox2d.dynamics.*;
+import org.jbox2d.dynamics.contacts.*;
+import static model.State.*;
+
+import p5.Area;
+
+public class P5Canvas extends Area {
+
+	// A reference to our box2d world
+	PBox2D box2d;
+	
+	public void setup() {
+		smooth();
+		setW(570);
+		setH(556);
+		setDimensions(0, 0, w(), h());  // this is a custom function from the Region interface, implemented in the Area class
+		size(floor(w()), floor(h()));
+		
+		// Initialize box2d physics and create the world
+		box2d = new PBox2D(this);
+		box2d.createWorld();
+		box2d.setGravity(0f,-10f);
+		// Turn on collision listening!
+		// TODO turn on collisions by uncommenting below
+		//box2d.listenForCollisions();
+
+		// Add a bunch of fixed boundaries
+		
+		float bW = 1; // boundary width
+		
+		Boundary lBound = new Boundary(x(), mh(), bW, h(), box2d, this);
+		Boundary rBound = new Boundary(r(), mh(), bW, h(), box2d, this);
+		Boundary tBound = new Boundary(mw(), y(), w(), bW, box2d, this);
+		Boundary bBound = new Boundary(mw(), b(), w(), bW, box2d, this);
+
+		boundaries[0]=lBound;
+		boundaries[1]=rBound;
+		boundaries[2]=tBound;
+		boundaries[3]=bBound;
+		
+	}
+
+	public void draw() {
+		drawBackground();
+		
+		// We must always step through time!
+		box2d.step();
+		
+		//if (w!=width || h!=height){
+		//	setup(width, height);
+		//}
+		
+		
+		// Display all molecules
+		for (int i = 0; i < molecules.size(); i++) {
+			Molecule m = molecules.get(i);
+			m.display();
+		}
+		// boundaries are not displayed.  If they should be, use a display method in the Boundary class.
+		// System.out.println("x: " + str(boundaries.get(2).x()) +" y: " + str(boundaries.get(2).y()) + " w: " + str(boundaries.get(2).w()) + "  h: " + str(boundaries.get(2).h()) );
+		
+	}
+	
+	/*
+	 * Background methods
+	 */
+	
+	private void drawBackground() { // draw background
+		pushStyle();
+		fill(127, 0, 0);
+		rect(x(), y(), w(), h());
+		popStyle();
+	}
+	
+	/*
+	 * Function to create compounds from outside the PApplet
+	 */
+	
+	public void addMolecule(String compoundName) {
+		float x_ = 200;
+		float y_ = random(200, y());
+		molecules.add(new Molecule(x_, y_,compoundName, box2d, this));
+	}
+	
+	public void addMolecule() {
+		addMolecule("Water");
+	}
+	
+	public void addMolecule(float x_, float y_) {
+		addMolecule(x_, y_, "Water");
+	}
+	
+	public void addMolecule(float x_, float y_, String compoundName) {
+		molecules.add(new Molecule(x_,y_,compoundName, box2d, this));
+	}
+	
+	public void mousePressed() {
+		addMolecule(mouseX, mouseY);
+	}
+	// Collision event functions!
+	public void addContact(ContactPoint cp) {
+		// Get both shapes
+		Shape s1 = cp.shape1;
+		Shape s2 = cp.shape2;
+		// Get both bodies
+		Body b1 = s1.getBody();
+		Body b2 = s2.getBody();
+		// Get our objects that reference these bodies
+		Object o1 = b1.getUserData();
+		Object o2 = b2.getUserData();
+
+		// What class are they?  Box or Particle?
+		String c1 = o1.getClass().getName();
+		String c2 = o2.getClass().getName();
+		// If object 1 is a Box, then object 2 must be a particle
+		// Note we are ignoring particle on particle collisions
+		if (c1.contains("Boundary") && ((Boundary)o1).equals(boundaries[0])) {
+			Molecule p = (Molecule) o2;
+		} 
+		// If object 2 is a Box, then object 1 must be a particle
+		else if (c2.contains("Boundary")&& ((Boundary)o2).equals(boundaries[0])) {
+			Molecule p = (Molecule) o1;
+		}
+	}
+}
