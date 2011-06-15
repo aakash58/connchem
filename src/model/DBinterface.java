65ca2669a75433d92a0749c87bd966adb38b6f0b
Tuan Dang <nhontuan@gmail.com>
2011-06-15 16:25:52 -0500

    Update initial settings

---
+++ src/model/DBinterface.java
@@ -0,0 +1,376 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+import java.sql.*;
+
+public class DBinterface {
+
+	public static ArrayList dbConnect(String[] args) {
+		ArrayList output = new ArrayList();
+		try {
+			Class.forName("org.sqlite.JDBC");
+			Connection conn = DriverManager.getConnection("jdbc:sqlite:src/model/chemdb");
+			Statement stat = conn.createStatement();
+
+			ResultSet rs = stat.executeQuery(args[0]);
+
+			while (rs.next()) {
+				output.add(rs.getString(args[1]));
+			}
+			rs.close();
+			conn.close();
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return output;
+	}
+
+	public static ArrayList getAllCompoundNames(String order_) {
+		ArrayList output = new ArrayList();
+
+		String[] args = new String[2];
+		args[0] = "SELECT * FROM compounds ORDER BY " + order_;
+		args[1] = "name";
+		output = dbConnect(args);
+
+		return output;
+	}
+
+	public static ArrayList getAllCompoundFormulas(String order_) {
+		ArrayList output = new ArrayList();
+
+		String[] args = new String[2];
+		args[0] = "SELECT * FROM compounds ORDER BY " + order_;
+		args[1] = "formula";
+		output = dbConnect(args);
+
+		return output;
+	}
+
+	public static ArrayList getAllElementNames(String order_) {
+		ArrayList output = new ArrayList();
+
+		String[] args = new String[2];
+		args[0] = "SELECT * FROM elements ORDER BY " + order_;
+		args[1] = "name";
+		output = dbConnect(args);
+
+		return output;
+	}
+
+	public static Float getElementMass(String elementName_) {
+
+		//ArrayList results = new ArrayList();
+		String[] args = new String[2];
+		args[0] = "SELECT mass FROM elements WHERE name = \"" + elementName_ + "\"";
+		args[1] = "mass";
+		ArrayList results = dbConnect(args);
+
+		try {
+			return Float.valueOf((String)results.get(0)).floatValue();
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+		}
+	}
+
+	public static Float getElementRadiusAtomic(String elementName_) {
+		String[] args = new String[2];
+		args[0] = "SELECT radiusAtomic FROM elements WHERE name = \"" + elementName_ + "\"";
+		args[1] = "radiusAtomic";
+		ArrayList results = dbConnect(args);
+
+		try {
+			return Float.valueOf((String)results.get(0)).floatValue();
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+		}
+	}
+
+	public static Float getElementRadiusIonic(String elementName_) {
+		String[] args = new String[2];
+		args[0] = "SELECT radiusIonic FROM elements WHERE name = \"" + elementName_ + "\"";
+		args[1] = "radiusIonic";
+		ArrayList results = dbConnect(args);
+
+		try {
+			return Float.valueOf((String)results.get(0)).floatValue();
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+		}
+	}
+
+	public static Float getElementArea(String elementName_) {
+		Float radius = getElementRadiusAtomic(elementName_);
+		return (float) (Math.PI * (radius * radius));
+	}
+
+	public static Float getElementAreaIonic(String elementName_) {
+		Float radius = getElementRadiusIonic(elementName_);
+		return (float) (Math.PI * (radius * radius));
+	}
+
+	public static Float getElementDensity(String elementName_) {
+		String[] args = new String[2];
+		args[0] = "SELECT density FROM elements WHERE name = \"" + elementName_ + "\"";
+		args[1] = "density";
+		ArrayList results = dbConnect(args);
+
+		try {
+			return Float.valueOf((String)results.get(0)).floatValue();
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+		}
+	}
+
+	public static ArrayList<String> getCompoundConstituentElements(String compoundName_) {
+		ArrayList<String> output = new ArrayList();
+
+		String[] args = new String[2];
+		args[0] = "SELECT E.name FROM compounds as C, compounds_elements as CE, elements as E WHERE CE.compound_id = C.id and CE.element_id = E.id and C.name = \"" + compoundName_ + "\"";
+		args[1] = "name";
+		output = dbConnect(args);
+
+		return output;
+	}
+
+	public static Float getCompoundMass(String compoundName_) {
+		ArrayList elementMasses = new ArrayList();
+		float mass = 0;
+
+		String[] args = new String[2];
+		args[0] = "SELECT E.mass, E.name FROM compounds as C, compounds_elements as CE, elements as E WHERE C.name = \"" + compoundName_ + "\" and C.id = CE.compound_id and E.id = CE.element_id";
+		args[1] = "mass";
+		elementMasses = dbConnect(args);
+
+		try {
+			for (int i = 0; i<elementMasses.size();i++) {
+				String elementMass = (String)elementMasses.get(i);
+				mass += Float.valueOf(elementMass);
+			}
+			return mass;
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+		}
+	}
+
+	public static String getCompoundFormula(String compoundName_) {
+		String formula = "blork";
+
+		String[] args = new String[2];
+		args[0] = "SELECT formula FROM compounds WHERE name = \"" + compoundName_ + "\"";
+		args[1] = "formula";
+		ArrayList results = dbConnect(args);
+
+		try {
+			return (String)results.get(0);
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+		}
+	}
+
+	public static Integer getCompoundCharge(String compoundName_) {
+		Integer charge = 0;
+
+		ArrayList results = new ArrayList();
+		String[] args = new String[2];
+		args[0] = "SELECT charge FROM compounds WHERE name = \"" + compoundName_ + "\"";
+		args[1] = "charge";
+		results = dbConnect(args);
+
+		try {
+			charge = Integer.valueOf((String)results.get(0)).intValue();
+			return charge;
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+		}
+	}
+
+	public static Float getCompoundBoilingPointCelsius(String compoundName_) {
+		Float boilPoint = 0.f;
+
+		ArrayList results = new ArrayList();
+		String[] args = new String[2];
+		args[0] = "SELECT boilingPointCelsius FROM compounds WHERE name = \"" + compoundName_ + "\"";
+		args[1] = "boilingPointCelsius";
+		results = dbConnect(args);
+		
+		try {
+			boilPoint = Float.valueOf((String) results.get(0)).floatValue();
+			return boilPoint;
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+		}
+	}
+
+	public static Float getCompoundBoilingPointKelvin(String compoundName_) {
+		Float c = getCompoundBoilingPointCelsius(compoundName_);
+		return c - 273.15f;
+	}
+
+	public static Float getCompoundFreezingPointCelsius(String compoundName_) {
+		Float freezePoint = 0.f;
+
+		ArrayList results = new ArrayList();
+		String[] args = new String[2];
+		args[0] = "SELECT freezingPointCelsius FROM compounds WHERE name = \"" + compoundName_ + "\"";
+		args[1] = "freezingPointCelsius";
+		results = dbConnect(args);
+		
+		try {
+			freezePoint = Float.valueOf((String) results.get(0)).floatValue();
+			return freezePoint;
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+		}
+	}
+
+	public static Float getCompoundFreezingPointKelvin(String compoundName_) {
+		Float c = getCompoundFreezingPointCelsius(compoundName_);
+		return c - 273.15f;
+	}
+
+	public static Float getCompoundDensity(String compoundName_) {
+		Float density = 0.f;
+
+		ArrayList results = new ArrayList();
+		String[] args = new String[2];
+		args[0] = "SELECT density FROM compounds WHERE name = \"" + compoundName_ + "\"";
+		args[1] = "density";
+		results = dbConnect(args);
+
+		try {
+			density = Float.valueOf((String) results.get(0)).floatValue();
+			return density;
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+		}
+		
+
+	}
+
+	public static Boolean getCompoundPolarity(String compoundName_) {
+
+		String[] args = new String[2];
+		args[0] = "SELECT polarity FROM compounds WHERE name = \"" + compoundName_ + "\"";
+		args[1] = "polarity";
+		ArrayList results = dbConnect(args);
+
+		try {
+			Integer truthy = Integer.valueOf((String)results.get(0)).intValue();
+			if (truthy == 1) {
+				return true;
+			} else {
+				return false;
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+		}
+	}
+
+	public static Integer getReactionNumber(ArrayList<String> reactants) {
+
+		String reactant;
+		String[] args = new String[2];
+		ArrayList possibleReactionsMatrix = new ArrayList();
+
+		// poll database for each reactant
+		for (int i = 0; i<reactants.size(); i++) {
+			reactant = reactants.get(i);
+			args[0] = "SELECT R.id FROM reactions as R, reactions_compounds as RC, compounds as C WHERE C.id = RC.compound_id and R.id = RC.reaction_id and C.name = \"" + reactant + "\" and RC.type = \"input\"";
+			args[1] = "id";
+			possibleReactionsMatrix.add(dbConnect(args));
+		}
+
+		// short circuit if no reactions match reactants
+		if (possibleReactionsMatrix.size() == 0) {
+			return null;
+		}
+
+		// make sure that a reaction is common for all reactants
+		ArrayList commonReactions = new ArrayList();
+		commonReactions = (ArrayList)possibleReactionsMatrix.get(0);
+		for (int i = 0; i<possibleReactionsMatrix.size(); i++) {
+			commonReactions.retainAll((Collection) possibleReactionsMatrix.get(i));
+		}
+
+		// short circuit if anything other than 1 reaction possible
+		if (commonReactions.size() != 1) {
+			return null;
+		}
+
+		String reaction = (String)commonReactions.get(0);
+
+		// short circuit if quantity of input reactants does not match required number 
+		args = new String[2];
+		args[0] = "SELECT C.name FROM reactions as R, reactions_compounds as RC, compounds as C WHERE C.id = RC.compound_id and R.id = RC.reaction_id and RC.type = \"input\" and R.id = " + reaction;
+		args[1] = "name";
+		if (dbConnect(args).size() != reactants.size()) {
+			return null;
+		}
+		return Integer.parseInt(reaction);
+	}
+
+	public static ArrayList<String> getReactionProducts(ArrayList<String> reactants) {
+		ArrayList<String> products = new ArrayList<String>();
+
+		Integer reaction = getReactionNumber(reactants);
+
+		// get products
+		String[] args = new String[2];
+		args[0] = "SELECT C.name FROM reactions as R, reactions_compounds as RC, compounds as C WHERE C.id = RC.compound_id and R.id = RC.reaction_id and RC.type = \"output\" and R.id = " + reaction;
+		args[1] = "name";
+		products = dbConnect(args);
+
+		// short circuit if no products
+		if (products.size() == 0) {
+			return null;
+		}
+		else {
+			return products;
+		}
+	}
+
+	public static Float getReactionProbability(ArrayList<String> reactants) {
+		Integer reaction = getReactionNumber(reactants);
+		Float defaultReactProb = 0.f;
+		
+		try {
+			return getReactionProbability(reaction);
+		} catch (Exception e) {
+			//System.out.println("Reaction invalid: " + e);
+			return defaultReactProb;
+		}
+	}
+
+	public static Float getReactionProbability(int reactionNumber_) {
+		Float defaultReactProb = 0.f;
+		
+		ArrayList results = new ArrayList();
+		String[] args = new String[2];
+		args[0] = "SELECT probability FROM reactions WHERE id = " + reactionNumber_;
+		args[1] = "probability";
+		results = dbConnect(args);
+
+		try {
+			return Float.parseFloat((String)results.get(0));
+		} catch (Exception e) {
+			//System.out.println("Reaction invalid: " + e);
+			return defaultReactProb;
+		}
+	}
+}
---
+++ src/view/P5Canvas.java
@@ -0,0 +1,525 @@
+package view;
+
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Dimension;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
+import pbox2d.*;
+import processing.core.PApplet;
+
+
+import main.Main;
+
+import org.jbox2d.collision.shapes.*;
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.*;
+import org.jbox2d.dynamics.contacts.*;
+
+import Util.ColorScales;
+import static model.State.*;
+import model.DBinterface;
+import model.YAMLinterface;
+
+public class P5Canvas extends PApplet{
+	public static float x;
+	public static float y;
+	public static float w;//width of the boundary
+	public static float h;//width of the boundary
+	public static float width;//width of this Panel
+	public static float height;//width of this  Panel
+	
+	// A reference to our box2d world
+	private PBox2D box2d = new PBox2D(this);
+	public static boolean isEnable = true;
+	public static int creationCount = 0;
+	public static float restitution =0.5f;
+	public static float gravity =1.f;
+	
+	//Default value of speed
+	public static float speedRate = 1.f;
+	//Default value of heat
+	public static float heatRate = 1.f;
+	//Default value of scale slider
+	public static float scale = 1.f;
+	//Default value of volume slider
+	public static int defaultVolume =50;
+	public static int currenttVolume =defaultVolume;
+	public static int multiplierVolume =10; // Multiplier from pixels to ml
+	public static float minH=0;//minimum height of container
+	public static float maxH=1100;//minimum height of container
+	
+	public static int heatRGB = 0;
+	
+	public static int count = 0;
+	public static int xStart = 0;
+	public static int yStart = 0;
+	public static int xDrag = 0;
+	public static int yDrag = 0;
+	public static boolean isDrag = false;
+	ArrayList<String> products = new ArrayList<String>();
+	ArrayList<Molecule> killingList = new ArrayList<Molecule>();
+	public static int draggingBoundary =-1;
+	private boolean isFirstTime =true;
+
+	/*
+	 * for testing
+	 */
+	public static DBinterface db = new DBinterface();
+	public static YAMLinterface yaml = new YAMLinterface();
+	
+	public void updateSize(Dimension d, int volume) {
+		boolean tmp = isEnable;
+		isEnable = false;
+		
+		//setBoundary(0,0,d.width,d.height);
+		width = d.width;
+		height = d.height;
+		minH = (volume - defaultVolume)*multiplierVolume;
+		maxH = (volume + defaultVolume)*multiplierVolume;
+		
+		isEnable = tmp;
+	}
+		
+	public void setup() {
+		smooth();
+		frameRate(24);
+		
+		// Initialize box2d physics and create the world
+		box2d.createWorld(-400,-400, 600, 600);
+		box2d.setGravity(0f,0f);
+		// Turn on collision listening!
+		// TODO turn on collisions by un-commenting below
+		box2d.listenForCollisions();
+		setBoundary(0,0,648,600);	
+		testDbInterface();
+	}
+	
+	private void testDbInterface() {
+		/*
+		ArrayList<String> reactants = new ArrayList<String>();
+		reactants.add("Hydrogen Peroxide");
+		reactants.add("Hydrogen Peroxide");
+		//reactants.add("Water");
+		//reactants.add("Ammonium");
+		
+		println(db.getReactionProducts(reactants));
+		println(db.getReactionProbability(10));*/
+		//System.out.println(db.getCompoundCharge("Copper-III"));
+		System.out.println("Water:   "+db.getCompoundMass("Water")+" "+db.getCompoundPolarity("Water"));
+		System.out.println("Bromine: "+db.getCompoundMass("Bromine") + " "+db.getCompoundPolarity("Bromine"));
+		System.out.println("Pantane: "+db.getCompoundMass("Pentane")+" "+db.getCompoundPolarity("Pentane"));
+		System.out.println("Mercury: "+db.getCompoundMass("Mercury")+ " "+db.getCompoundPolarity("Mercury"));
+		System.out.println(db.getCompoundDensity("Hydrogen-Peroxide"));
+		
+	}
+	
+	
+	public void setBoundary(float xx, float yy, float ww, float hh) {
+		if (hh<minH || hh>maxH) return;
+		x=xx;
+		y=yy;
+		w = ww;
+		h = hh;
+		if (isFirstTime){
+			size((int) w, (int) h);
+			isFirstTime =false;
+		}
+		
+		// Add a bunch of fixed boundaries
+		float bW = 10.f; // boundary width
+		int sliderValue = Main.volumeSlider.getValue();
+		Boundary lBound = new Boundary(0,x 	,  y+minH/2 , bW, 2*h -minH , sliderValue, box2d, this);
+		Boundary rBound = new Boundary(1,x+w , y+minH/2 , bW, 2*h -minH, sliderValue, box2d, this);
+		Boundary tBound = new Boundary(2,x+w/2, y,     w +bW , bW, sliderValue, box2d, this);
+		Boundary bBound = new Boundary(3,x+w/2, y+h,   w +bW , bW, sliderValue, box2d, this);
+		if (boundaries[0] != null)
+			boundaries[0].killBody();
+		if (boundaries[1] != null)
+			boundaries[1].killBody();
+		if (boundaries[2] != null)
+			boundaries[2].killBody();
+		if (boundaries[3] != null)
+			boundaries[3].killBody();
+		boundaries[0]=lBound;
+		boundaries[1]=rBound;
+		boundaries[2]=tBound;
+		boundaries[3]=bBound;
+	}
+	
+		
+	public void draw() {
+		if (getSize().width != w || getSize().height!=h){
+			//setBoundary(0,0,this.getSize().width,this.getSize().height);
+		}
+		
+		drawBackground();
+		// We must always step through time!
+		if (products!=null && products.size()>0){
+			Molecule m1 = (Molecule) killingList.get(0);
+			Molecule m2 = (Molecule) killingList.get(1);
+			for (int i=0;i<products.size();i++){
+				Vec2 loc =m1.getPosition();
+				addMolecule(box2d.scalarWorldToPixels(loc.x),box2d.scalarWorldToPixels(loc.y),products.get(i));
+			}
+			m1.killBody();
+			m2.killBody();
+			molecules.remove(m1);
+			molecules.remove(m2);
+			
+			products = new ArrayList<String>();
+			killingList = new ArrayList();
+		}
+		
+			if (isEnable){
+				float timeStep = speedRate / 60.0f;
+				box2d.step(timeStep,20);
+			}	
+		
+		
+		this.scale(scale);
+		
+		// Apply gravity to molecules
+		for (int i = 0; i < molecules.size(); i++) {
+			Molecule m = molecules.get(i);
+			if (m!=null){
+				m.setRestitution(restitution);
+				setForce(i,m);
+			}	
+		}
+		// Display all molecules
+		for (int i = 0; i < molecules.size(); i++) {
+			Molecule m = molecules.get(i);
+			m.display();
+		}
+		boundaries[0].display();
+		boundaries[1].display();
+		boundaries[2].display();
+		boundaries[3].display();
+			
+		computeEnergy();
+	}
+	private void computeEnergy(){
+		float sum = 0f;
+		for (int i = 0; i < molecules.size(); i++) {
+			Molecule m = molecules.get(i);
+			Vec2 vec = m.getSpeed();
+			if (vec!=null){
+				float v = vec.x*vec.x + vec.y*vec.y;
+				sum += v*m.getMass();
+			}
+		}
+		
+		float average =0f;
+		if (molecules.size()>0)
+			average = sum/molecules.size();
+		
+		if (Main.totalSystemEnergy!=null){
+			DecimalFormat df = new DecimalFormat("#.##");
+			Main.totalSystemEnergy.setText(df.format(sum)+" kJ");
+			Main.averageSystemEnergy.setText(df.format(average )+" kJ");
+		}	
+	}
+	
+	private void setForce(int index, Molecule mIndex) { // draw background
+		for (int i = 0; i < molecules.size(); i++) {
+			if (i==index)
+				continue;
+			Molecule m = molecules.get(i);
+			Vec2 loc = m.getPosition();
+			Vec2 locIndex = mIndex.getPosition();
+			if(loc==null || locIndex==null) continue;
+			float x = locIndex.x-loc.x;
+			float y = locIndex.y-loc.y;
+			float dis = x*x +y*y;
+			Vec2 normV = normalizeForce(new Vec2(x,y));
+			float forceX;
+			float forceY;
+			
+			if (mIndex.polarity==m.polarity){
+				forceX =  (-normV.x/dis)*m.getMass()*mIndex.getMass()*gravity;
+				forceY =  (-normV.y/dis)*m.getMass()*mIndex.getMass()*gravity;
+			}	
+			else{
+				forceX =  (normV.x/dis)*m.getMass()*mIndex.getMass()*gravity;
+				forceY =  (normV.y/dis)*m.getMass()*mIndex.getMass()*gravity;
+			}
+			mIndex.addForce(new Vec2(forceX,forceY));
+			
+		}
+					
+	}
+		
+	private Vec2 normalizeForce(Vec2 v){
+		float dis = (float) Math.sqrt(v.x*v.x + v.y*v.y);
+		return new Vec2(v.x/dis,v.y/dis);
+		
+	}
+	
+	/*
+	 * Background methods
+	 */
+	private void drawBackground() { // draw background
+		count++;
+		if (count>10000)
+			count =0;
+		Main.canvas.repaint();
+		pushStyle();
+		fill(127, 127, 127);
+		rect(0, 0, width, height);
+		popStyle();
+	}
+	
+	
+	/*
+	 * Function to create compounds from outside the PApplet
+	 */
+	public void addMolecule(String compoundName, int count) {
+		// The tmp variable helps to fix a Box2D Bug: 2147483647  because of Multithreading
+		// at pbox2d.PBox2D.step(PBox2D.java:81)
+		// at pbox2d.PBox2D.step(PBox2D.java:72)
+		// at pbox2d.PBox2D.step(PBox2D.java:67)
+		// at view.P5Canvas.draw(P5Canvas.java:73)
+		boolean tmp = isEnable;
+		isEnable = false;
+		
+		if (tmp) //if Applet is enable
+			creationCount =0;
+		else
+			creationCount++;
+									// variables are used to distribute molecules
+		int mod = creationCount%4;  // When the system is paused; Otherwise, molecules are create at the same position
+		for (int i=0;i<count;i++){
+			float x_ =x + w/2 +40+ (i-count/2.f)*(w/11) + creationCount;
+			float y_ =y + 80-Boundary.difVolume +(mod-1.5f)*20;
+			Vec2 newVec =removeDuplicatePosition(new Vec2(x_,y_));
+			molecules.add(new Molecule(newVec.x, newVec.y,compoundName, box2d, this));
+		}
+		isEnable = tmp;
+	}
+	public void addMoleculeRandomly(String compoundName, int count) {
+		boolean tmp = isEnable;
+		isEnable = false;
+		
+		float PAD =60;
+		for (int i=0;i<count;i++){
+			float x_ = this.random(PAD, w-PAD);
+			float y_ = this.random(PAD, h-PAD);
+			Vec2 newVec =removeDuplicatePosition(new Vec2(x_,y_));
+			molecules.add(new Molecule(newVec.x, newVec.y,compoundName, box2d, this));
+		}
+		isEnable = tmp;
+	}
+		
+	public boolean isDuplicatePosition(Vec2 v) {
+		for (int i = 0; i < molecules.size(); i++) {
+			Molecule m = molecules.get(i);
+			if (m!=null){
+				Vec2 vec = box2d.coordWorldToPixels(m.getPosition());
+				if (v.x-1 <= vec.x && vec.x<=v.x+1 && v.y-1<=vec.y && vec.y<=v.y+1)
+					return true;
+			}	
+		}
+		return false;
+	}
+	public Vec2 removeDuplicatePosition(Vec2 v) {
+		while (isDuplicatePosition(v)){
+			v.x=v.x+5;
+			v.y=v.y+5;
+		}
+		return v;
+	}	
+	
+	public void addMolecule(float x_, float y_, String compoundName) {
+		Vec2 newVec =removeDuplicatePosition(new Vec2(x_,y_));
+		Molecule m = new Molecule(newVec .x, newVec.y,compoundName, box2d, this);
+		molecules.add(m);
+ 	}
+	
+	public void removeAllMolecules() {
+		boolean tmp = isEnable;
+		isEnable = false;
+		
+		for (int i =0; i< molecules.size(); i++){
+			Molecule m = (Molecule) molecules.get(i);
+			m.killBody();
+		}
+		molecules =  new ArrayList();
+		
+		isEnable = tmp;
+	}
+	
+	//Set Speed of Molecules; values are from 0 to 100; 20 is default value 
+	public void setSpeed(float speed) {
+		speedRate = speed;
+	}
+	
+	
+	//Set Speed of Molecules; values are from 0 to 100; 50 is default value 
+	public void setHeat(int value) {
+		restitution = (float) (value)/50;
+		restitution = 0.5f+restitution*0.5f;
+		gravity = (100f-value)/10f;
+		//gravity = (float) Math.pow(gravity, 1.1);
+	
+		double v = (double) value/100;
+		Color color = ColorScales.getColor(1-v, "redblue", 1f);
+		heatRGB = color.getRGB();
+	}
+	
+	//Set Scale of Molecules; values are from 0 to 100; 50 is default value 
+	public void setScale(int value, int defaultScale) {
+		boolean tmp = isEnable;
+		isEnable = false;
+		scale = (float) value/defaultScale;
+		isEnable = tmp;
+	}
+	
+	//Set Volume; values are from 0 to 100; 50 is default value 
+	public void setVolume(int value) {
+		boolean tmp = isEnable;
+		isEnable = false;
+		//int dif = value-currenttVolume;
+		boundaries[2].set(value);
+		
+		currenttVolume = value;
+		isEnable = tmp;
+	}
+	
+	
+	//********************************* MOUSE EVENT ******************************
+	public void mouseMoved() {		
+		//Check the top boundary
+		int id = boundaries[2].isIn(mouseX, mouseY);
+		if (id==2)
+			this.cursor(Cursor.N_RESIZE_CURSOR);
+		else
+			this.cursor(Cursor.DEFAULT_CURSOR);
+	}
+		
+	public void mousePressed() {
+		xStart = mouseX;
+		yStart = mouseY;
+		draggingBoundary = boundaries[2].isIn(mouseX, mouseY);
+	}
+	
+	public void mouseReleased() {
+		xDrag =0;
+		yDrag =0;
+		isDrag = false;
+		draggingBoundary =-1;
+		
+		//Check the top boundary
+	/*	int id = boundaries[2].isIn(mouseX, mouseY);
+		if (id==2)
+			this.cursor(Cursor.N_RESIZE_CURSOR);
+		else
+			this.cursor(Cursor.DEFAULT_CURSOR);*/
+	}
+	
+	public void mouseDragged() {
+		isDrag = true;
+			
+		int xTmp = xDrag;
+		int yTmp = yDrag;
+		xDrag = (int) ((mouseX-xStart)/scale);
+		yDrag = (int) ((mouseY-yStart)/scale);
+		
+		//Dragging the top boundary
+		if (draggingBoundary==2){
+		/*	setBoundary(x,y+yDrag-yTmp,w,h-(yDrag-yTmp));
+			//Update volume label
+			int volume = (int) h/10;
+			Main.volumeLabel.setText(volume +" ml");
+			//Update volume slider
+			int value = (int) (h-minH)/10;
+			Main.isVolumeblocked =true;
+			Main.volumeSlider.setValue(value);
+			Main.isVolumeblocked =false;
+			*/
+		}	
+		else{
+			setBoundary(x+xDrag -xTmp,y+yDrag - yTmp,w,h);
+		}
+	}
+		
+	public void mouseClicked() {
+		addMolecule(mouseX/scale, mouseY/scale,"Water");
+	}
+	
+	
+	
+	
+	// A FAKE function for testing
+	private ArrayList<String> getReactionProducts(ArrayList<String> reactants) {	
+		if (reactants.get(0).equals("Hydrogen-Peroxide") &&
+			reactants.get(1).equals("Hydrogen-Peroxide")){
+			ArrayList<String> products = new ArrayList<String>();
+			products.add("Water");
+			products.add("Water");
+			products.add("Oxygen");
+			
+			return products;
+		}
+		else{
+			return null;
+		}
+	}
+	// Collision event functions!
+	public void addContact(ContactPoint cp) {
+		//System.out.println("Contact heatRate: "+heatRate);
+		//System.out.println(Main.leftPanel.getSize()+" "+Main.centerPanel+" "+Main.rightPanel.getSize());
+		// Get both shapes
+		Shape s1 = cp.shape1;
+		Shape s2 = cp.shape2;
+		// Get both bodies
+		Body b1 = s1.getBody();
+		Body b2 = s2.getBody();
+		// Get our objects that reference these bodies
+		Object o1 = b1.getUserData();
+		Object o2 = b2.getUserData();
+
+		// What class are they?  Box or Particle?
+		String c1 = o1.getClass().getName();
+		String c2 = o2.getClass().getName();
+		// If object 1 is a Box, then object 2 must be a particle
+		// Note we are ignoring particle on particle collisions
+		if (c1.contains("Boundary")) {
+			Boundary b = (Boundary) o1;
+			if (b.getId()==3){
+				Molecule p = (Molecule) o2;
+				//p.setSpeedByHeat(1.5f);
+			}
+			
+			
+		} 
+		// If object 2 is a Box, then object 1 must be a particle
+		else if (c2.contains("Boundary")) {
+			Molecule p = (Molecule) o1;
+			//p.setSpeedByHeat(heatRate);
+		}
+		else if (c1.contains("Molecule") && c2.contains("Molecule")){
+			Molecule m1 = (Molecule) o1;
+			Molecule m2 = (Molecule) o2;
+			ArrayList<String> reactants = new ArrayList<String>();
+			reactants.add(m1.getName());
+			reactants.add(m2.getName());
+			//products = getReactionProducts(reactants);
+			if (products!=null && products.size()>0){
+				killingList.add(m1);
+				killingList.add(m2);
+			}
+			
+			
+		}
+	}
+	
+	public void persistContact(ContactPoint cp) {
+	}
+	public void removeContact(ContactPoint cp) {
+	}
+	public void resultContact(ContactResult cr) {
+	}
+		
+}
