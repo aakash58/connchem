20af386ca98e7cc6bed6b8b033a3245ebad42337
Qin Li <Qin Li@localhost>
2012-09-07 18:23:47 -0500

    Finished Unit 9 Sim 1 Set 1

--- src/Util/MathFunction.java
+++ src/Util/MathFunction.java
@@ -15,5 +15,18 @@ public class MathFunction {
 		float dis = (float)Math.sqrt(v.x * v.x + v.y * v.y);
 		return new Vec2(v.x / dis, v.y / dis);
 	}
+	
+	public static float computeDistance(Vec2 v1, Vec2 v2) {
+		float dis = 0;
+		dis = (v1.x - v2.x) * (v1.x - v2.x) + (v1.y - v2.y) * (v1.y - v2.y);
+		dis = (float) Math.sqrt(dis);
+
+		return dis;
+	}
+	
+	public static float norm(Vec2 v)
+	{
+		return (float) Math.sqrt(v.x*v.x + v.y * v.y );
+	}
 
 }
